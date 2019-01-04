d0864dd5f34d9300ade291cc3700ee8ab2fda36c
Kam <Kam@Bansal-Master>
2019-01-04 23:21:43 +0000

    update

--- Test/src/booking/Codes.java
+++ Test/src/booking/Codes.java
@@ -1,5 +1,27 @@
-package booking;
-
-public class Codes {
-
-}
+/*
+ * package booking;
+ * 
+ * public class Codes {
+ * 
+ * Sum of all numbers
+ * 
+ * Return (n * (n+1)) /2
+ ** 
+ * 
+ * 
+ * Factorial – product of all numbers If (n == 0 ) {
+ * 
+ * Return 1; (because factorial of 0! = 1 } Long fact = 1;
+ * 
+ * For (int I = o; I <=n; i++) { Fact = fact * I; } Return fact; }
+ ** 
+ * Fibonacci If (n ==0) { return 0; } else if (n==1) { return 1; } long nMinus1
+ * = 1; long nMinus2 = 0; long fib = 0;
+ * 
+ * for (int I = 1; I < n; i++) { fib = (nMinus2 + nMinus 1); nMinus 2 = nMinus
+ * 1; nMinus 1 = fib; } Return fib;
+ * 
+ * 
+ * 
+ * }
+ */
--- Test/src/booking/RoomBooking.java
+++ Test/src/booking/RoomBooking.java
@@ -1,4 +1,4 @@
-package room;
+package booking;
 
 import java.util.HashMap;
 
